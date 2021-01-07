ee27118fa3439e84d769276941fbb1651c1aa1f7
TzeHimSung <zxc98567@163.com>
2021-01-07 21:03:55 +0800

    build(src/main.js package.json package-lock.json): add Element.ui component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+// CSS configuration of element-ui is needed to be imported
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
