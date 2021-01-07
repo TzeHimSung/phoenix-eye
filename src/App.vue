73206b6781062fc38f7ee1b5d734eb0417389896
TzeHimSung <zxc98567@163.com>
2021-01-07 19:56:16 +0800

    feat(app.vue): add component data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <HelloWorld2 v-bind:msg="helloWorld2msg" />
+
     <div>
       <p v-if="pSeen">now you can see this p label</p>
+    </div>
+
+    <div>
+      <ol>
+        <li v-for="todo in todos" :key="todo.id">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
+
+    <div>
+      <p>{{ helloWorld2msg }}</p>
+      <button @click="reverseMessage">Reverse message</button>
+    </div>
+
+    <div>
+      <p>{{ inputMsg }}</p>
+      <input v-model="inputMsg">
     </div>
   </div>
 </template>
@@ -22,7 +42,18 @@ export default {
   data () {
     return {
       helloWorld2msg: 'this is text message for Helloworld2',
-      pSeen: true
+      pSeen: true,
+      todos: [
+        { id: 1, text: 'Learn JavaScript' },
+        { id: 2, text: 'Learn Vue' },
+        { id: 3, text: 'Make a big deal' }
+      ],
+      inputMsg: 'same message'
+    }
+  },
+  methods: {
+    reverseMessage () {
+      this.helloWorld2msg = this.helloWorld2msg.split('').reverse().join('')
     }
   }
 }
