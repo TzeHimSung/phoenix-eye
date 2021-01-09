7fe7e4a00a841c768d5be9aa0d3b1df999e5216c
TzeHimSung <zxc98567@163.com>
2021-01-09 14:15:08 +0800

    refactor(app.vue app.css main.js): adjust CSS configuration to App.css

--- src/App.vue
+++ src/App.vue
@@ -33,15 +33,4 @@
 
 <script src="./App.js" />
 
-<style>
-#app {
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+<style src="./App.css" />
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
 
+// 阻止 vue 在启动时生成生产提示
 Vue.config.productionTip = false
 
 new Vue({
