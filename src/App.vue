3a632df3043ef15b5aa5191d475e18ccdc679b56
TzeHimSung <zxc98567@163.com>
2021-01-09 12:58:51 +0800

    feat(src/app.vue src/app.js): move JS code from App.vue to App.js

--- src/App.vue
+++ src/App.vue
@@ -31,39 +31,7 @@
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import HelloWorld2 from './components/HelloWorld2.vue'
-import ElementContainer from './components/ElementContainer.vue'
-import NavMenuOne from './components/NavMenuOne.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    HelloWorld2,
-    ElementContainer,
-    NavMenuOne
-  },
-  data () {
-    return {
-      helloWorld2msg: 'this is text message for Helloworld2',
-      pSeen: true,
-      todos: [
-        { id: 1, text: 'Learn JavaScript' },
-        { id: 2, text: 'Learn Vue' },
-        { id: 3, text: 'Make a big deal' }
-      ],
-      inputMsg: 'same message'
-    }
-  },
-  methods: {
-    reverseMessage () {
-      this.helloWorld2msg = this.helloWorld2msg.split('').reverse().join('')
-    }
-  }
-}
-</script>
+<script src="./App.js" />
 
 <style>
 #app {
