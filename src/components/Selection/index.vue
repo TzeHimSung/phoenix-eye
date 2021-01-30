f18a2e6ec0240d49202192e527fcdc21a6ce8e51
TzeHimSung <zxc98567@163.com>
2021-01-30 18:57:46 +0800

    feat(src/components/selection/index.vue): add self-define selection component

---
+++ src/components/Selection/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="selection">
+    <label id="titleStyle">{{ title }} :</label>
+    <bk-select
+      id="selectStyle"
+      :disabled="false"
+      v-model="value"
+      style="width: 250px; display: inline-block;"
+      searchable
+    >
+      <bk-option
+        v-for="option in listData"
+        :key="option.id"
+        :id="option.id"
+        :name="option.name"
+      ></bk-option>
+    </bk-select>
+  </div>
+</template>
+
+<script>
+import { bkSelect, bkOption } from 'bk-magic-vue'
+
+export default {
+  name: 'selection',
+  components: {
+    bkSelect,
+    bkOption
+  },
+  props: {
+    title: String,
+    list: Array
+  },
+  data () {
+    return {
+      value: '',
+      listData: this.list
+    }
+  }
+}
+</script>
+
+<style>
+#selection {
+  text-align: center;
+  margin: 10px;
+}
+
+#labelStyle {
+  vertical-align: middle;
+}
+
+#selectStyle {
+  vertical-align: middle;
+  margin-left: 20px;
+}
+</style>
