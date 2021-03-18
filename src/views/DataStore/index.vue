46e9fbbfa406f42aa962f5631ee0ff41b03ba437
TzeHimSung <zxc98567@163.com>
2021-03-18 21:48:31 +0800

    feat(datastore): add file download

--- src/views/DataStore/index.vue
+++ src/views/DataStore/index.vue
@@ -1,23 +1,64 @@
 <template>
   <div id="DataStore">
     <div id="Selection">
-      <selection title="项目" :list="projectList"></selection>
-      <selection title="后缀名" :list="fileSuffixList"></selection>
+      <selection
+        title="项目"
+        :list="projectList"
+      ></selection>
+      <selection
+        title="后缀名"
+        :list="fileSuffixList"
+      ></selection>
     </div>
     <div id="DataTable">
-      <bk-table style="margin-top: 15px" :data="tableData" :size="'small'" :pagination="pagination"
-        @page-change="handlePageChange" @page-limit-change="handlePageLimitChange">
-        <bk-table-column type="index" label="ID" width="60"></bk-table-column>
-        <bk-table-column label="文件名" prop="fileName"></bk-table-column>
-        <bk-table-column label="来源" prop="source"></bk-table-column>
-        <bk-table-column label="状态" prop="status"></bk-table-column>
-        <bk-table-column label="创建时间" prop="createTime"></bk-table-column>
-        <bk-table-column label="操作" width="150">
+      <bk-table
+        style="margin-top: 15px"
+        :data="tableData"
+        :size="'small'"
+        :pagination="pagination"
+        @page-change="handlePageChange"
+        @page-limit-change="handlePageLimitChange"
+      >
+        <bk-table-column
+          type="index"
+          label="ID"
+          width="60"
+        ></bk-table-column>
+        <bk-table-column
+          label="文件名"
+          prop="fileName"
+        ></bk-table-column>
+        <bk-table-column
+          label="来源"
+          prop="source"
+        ></bk-table-column>
+        <bk-table-column
+          label="状态"
+          prop="status"
+        ></bk-table-column>
+        <bk-table-column
+          label="创建时间"
+          prop="createTime"
+        ></bk-table-column>
+        <bk-table-column
+          label="操作"
+          width="150"
+        >
           <template slot-scope="props">
-            <bk-button class="mr10" theme="primary" text :disabled="props.row.status === '上传中'"
-              @click="reset(props.row)">下载
+            <bk-button
+              class="mr10"
+              theme="primary"
+              text
+              :disabled="props.row.status === '上传中'"
+              @click="download(props.row)"
+            >下载
             </bk-button>
-            <bk-button class="mr10" theme="primary" text @click="remove(props.row)">删除</bk-button>
+            <bk-button
+              class="mr10"
+              theme="primary"
+              text
+              @click="remove(props.row)"
+            >删除</bk-button>
           </template>
         </bk-table-column>
       </bk-table>
@@ -44,7 +85,13 @@
 <script>
 import axios from 'axios'
 import selection from '@/components/Selection'
-import { bkTable, bkTableColumn, bkButton, bkUpload, bkDivider } from 'bk-magic-vue'
+import {
+  bkTable,
+  bkTableColumn,
+  bkButton,
+  bkUpload,
+  bkDivider
+} from 'bk-magic-vue'
 
 export default {
   name: 'DataStore',
@@ -58,10 +105,10 @@ export default {
   },
   created () {
     // get data store information
-    axios.get('http://localhost:8000/api/getDataStoreInfo').then(res => {
-      this.tableData = res.data['dataStoreInfo']
-      this.projectList = res.data['projectList']
-      this.fileSuffixList = res.data['fileSuffixList']
+    axios.get('http://localhost:8000/api/getDataStoreInfo').then((res) => {
+      this.tableData = res.data.dataStoreInfo
+      this.projectList = res.data.projectList
+      this.fileSuffixList = res.data.fileSuffixList
     })
   },
   data () {
@@ -117,6 +164,32 @@ export default {
     handleRes (response) {
       // 在这里处理返回结果，目前固定为成功
       return true
+    },
+    download (row) {
+      console.log(row)
+      // const params = {
+      //   headers: { 'X-Requested-With': 'XMLHttpRequest' }
+      // }
+      const data = {
+        filename: row.fileName
+      }
+      axios.post('http://localhost:8000/api/downloadData', data).then((res) => {
+        const content = res
+        const blob = new Blob([content])
+        const filename = data.filename
+        if ('download' in document.createElement('a')) {
+          const elink = document.createElement('a')
+          elink.download = filename
+          elink.style.display = 'none'
+          elink.href = URL.createObjectURL(blob)
+          document.body.appendChild(elink)
+          elink.click()
+          URL.revokeObjectURL(elink.href)
+          document.body.removeChild(elink)
+        } else {
+          navigator.msSaveBlob(blob, filename)
+        }
+      })
     }
   }
 }
