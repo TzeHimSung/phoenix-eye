92408eb6d01394e44851a71540733a782cff49d7
TzeHimSung <zxc98567@163.com>
2021-03-18 21:49:51 +0800

    style(app.vue home.vue modelstore): fix style with eslint

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,26 @@
       :default-open="false"
       :navigation-type="'top-bottom'"
       :need-menu=false
-      @toggle="handleToggle">
+      @toggle="handleToggle"
+    >
       <template slot="header">
         <div class="monitor-navigation-header">
           <ol class="header-nav">
-            <bk-popover v-for="(item,index) in header.list" :key="item.id" theme="light navigation-message" :arrow="false" offset="0, -5"  placement="bottom" :tippy-options="{ 'hideOnClick': false, flipBehavior: ['bottom'] }">
-              <li v-show="item.show" class="header-nav-item" :class="{ 'item-active': index === header.active }" @click="changeRouterView(item.name)">
+            <bk-popover
+              v-for="(item,index) in header.list"
+              :key="item.id"
+              theme="light navigation-message"
+              :arrow="false"
+              offset="0, -5"
+              placement="bottom"
+              :tippy-options="{ 'hideOnClick': false, flipBehavior: ['bottom'] }"
+            >
+              <li
+                v-show="item.show"
+                class="header-nav-item"
+                :class="{ 'item-active': index === header.active }"
+                @click="changeRouterView(item.name)"
+              >
                 {{item.name}}
               </li>
               <!-- 如果要做导航按钮二级菜单，反注释下面的代码 -->
@@ -25,15 +39,24 @@
             </bk-popover>
           </ol>
           <div style="margin-left: auto;">
-            <bk-popover theme="light navigation-message" :arrow="false" offset="-20, 10" placement="bottom-start"
-              :tippy-options="{ 'hideOnClick': false }">
+            <bk-popover
+              theme="light navigation-message"
+              :arrow="false"
+              offset="-20, 10"
+              placement="bottom-start"
+              :tippy-options="{ 'hideOnClick': false }"
+            >
               <div class="header-user">
                 Admin
                 <i class="bk-icon icon-down-shape"></i>
               </div>
               <template slot="content">
                 <ul class="monitor-navigation-admin">
-                  <li class="nav-item" v-for="userItem in user.list" :key="userItem">
+                  <li
+                    class="nav-item"
+                    v-for="userItem in user.list"
+                    :key="userItem"
+                  >
                     {{userItem}}
                   </li>
                 </ul>
@@ -43,7 +66,7 @@
         </div>
       </template>
       <div class="monitor-navigation-content">
-        <router-view/>
+        <router-view />
       </div>
       <template slot="footer">
         <div class="monitor-navigation-footer">
@@ -159,193 +182,193 @@ export default {
 
 <style>
 .monitor-navigation-header {
-  -webkit-box-flex:1;
-  -ms-flex:1;
-  flex:1;
-  height:100%;
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-align:center;
-  -ms-flex-align:center;
-  align-items:center;
-  font-size:14px;
+  -webkit-box-flex: 1;
+  -ms-flex: 1;
+  flex: 1;
+  height: 100%;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  font-size: 14px;
 }
 .monitor-navigation-header .header-nav {
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  padding:0;
-  margin:0;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  padding: 0;
+  margin: 0;
 }
 .monitor-navigation-header .header-nav-item {
-  list-style:none;
-  height:50px;
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-align:center;
-  -ms-flex-align:center;
-  align-items:center;
-  margin-right:40px;
-  color:#96A2B9;
-  min-width:56px
+  list-style: none;
+  height: 50px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  margin-right: 40px;
+  color: #96a2b9;
+  min-width: 56px;
 }
 .monitor-navigation-header .header-nav-item.item-active {
-  color:#FFFFFF !important;
+  color: #ffffff !important;
 }
 .monitor-navigation-header .header-nav-item:hover {
-  cursor:pointer;
-  color:#D3D9E4;
+  cursor: pointer;
+  color: #d3d9e4;
 }
 .monitor-navigation-header .header-title {
-  color:#63656E;
-  font-size:16px;
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-align:center;
-  -ms-flex-align:center;
-  align-items:center;
-  margin-left:-6px;
+  color: #63656e;
+  font-size: 16px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  margin-left: -6px;
 }
 .monitor-navigation-header .header-title-icon {
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-align:center;
-  -ms-flex-align:center;
-  align-items:center;
-  width:28px;
-  height:28px;
-  font-size:28px;
-  color:#3A84FF;
-  cursor:pointer;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  width: 28px;
+  height: 28px;
+  font-size: 28px;
+  color: #3a84ff;
+  cursor: pointer;
 }
 .monitor-navigation-header .header-user {
-  height:100%;
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-align:center;
-  -ms-flex-align:center;
-  align-items:center;
-  -webkit-box-pack:center;
-  -ms-flex-pack:center;
-  justify-content:center;
-  color:#96A2B9;
+  height: 100%;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  color: #96a2b9;
   margin-left: auto;
   margin-right: 8px;
 }
 .monitor-navigation-header .header-user .bk-icon {
-  margin-left:5px;
-  font-size:12px;
+  margin-left: 5px;
+  font-size: 12px;
 }
 .monitor-navigation-header .header-user:hover {
-  cursor:pointer;
-  color:#D3D9E4;
+  cursor: pointer;
+  color: #d3d9e4;
 }
 .monitor-navigation-content {
   min-height: 90%;
-  background:#FFFFFF;
-  -webkit-box-shadow:0px 2px 4px 0px rgba(25,25,41,0.05);
-  box-shadow:0px 2px 4px 0px rgba(25,25,41,0.05);
-  border-radius:2px;
-  border:1px solid rgba(220,222,229,1);
+  background: #ffffff;
+  -webkit-box-shadow: 0px 2px 4px 0px rgba(25, 25, 41, 0.05);
+  box-shadow: 0px 2px 4px 0px rgba(25, 25, 41, 0.05);
+  border-radius: 2px;
+  border: 1px solid rgba(220, 222, 229, 1);
   text-align: center;
   padding: 1%;
 }
 .monitor-navigation-footer {
-  height:52px;
-  width:100%;
-  margin:32px 0 0;
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-align:center;
-  -ms-flex-align:center;
-  align-items:center;
-  -webkit-box-pack:center;
-  -ms-flex-pack:center;
-  justify-content:center;
-  border-top:1px solid #DCDEE5;
-  color:#63656E;
-  font-size:12px;
+  height: 52px;
+  width: 100%;
+  margin: 32px 0 0;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  border-top: 1px solid #dcdee5;
+  color: #63656e;
+  font-size: 12px;
 }
 .monitor-navigation-nav {
-  width:150px;
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-orient:vertical;
-  -webkit-box-direction:normal;
-  -ms-flex-direction:column;
-  flex-direction:column;
-  background:#FFFFFF;
-  border:1px solid #E2E2E2;
-  -webkit-box-shadow:0px 3px 4px 0px rgba(64,112,203,0.06);
-  box-shadow:0px 3px 4px 0px rgba(64,112,203,0.06);
-  padding:6px 0;
-  margin:0;
-  color:#63656E;
+  width: 150px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  background: #ffffff;
+  border: 1px solid #e2e2e2;
+  -webkit-box-shadow: 0px 3px 4px 0px rgba(64, 112, 203, 0.06);
+  box-shadow: 0px 3px 4px 0px rgba(64, 112, 203, 0.06);
+  padding: 6px 0;
+  margin: 0;
+  color: #63656e;
 }
 .monitor-navigation-nav .nav-item {
-  -webkit-box-flex:0;
-  -ms-flex:0 0 32px;
-  flex:0 0 32px;
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-align:center;
-  -ms-flex-align:center;
-  align-items:center;
-  padding:0 20px;
-  list-style:none
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 32px;
+  flex: 0 0 32px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  padding: 0 20px;
+  list-style: none;
 }
 .monitor-navigation-nav .nav-item:hover {
-  color:#3A84FF;
-  cursor:pointer;
-  background-color:#F0F1F5;
+  color: #3a84ff;
+  cursor: pointer;
+  background-color: #f0f1f5;
 }
 .monitor-navigation-admin {
-  width:170px #63656E;
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-orient:vertical;
-  -webkit-box-direction:normal;
-  -ms-flex-direction:column;
-  flex-direction:column;
-  background:#FFFFFF;
-  border:1px solid #E2E2E2;
-  -webkit-box-shadow:0px 3px 4px 0px rgba(64,112,203,0.06);
-  box-shadow:0px 3px 4px 0px rgba(64,112,203,0.06);
-  padding:6px 0;
-  margin:0;
-  color:#63656E;
+  width: 170px #63656e;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  background: #ffffff;
+  border: 1px solid #e2e2e2;
+  -webkit-box-shadow: 0px 3px 4px 0px rgba(64, 112, 203, 0.06);
+  box-shadow: 0px 3px 4px 0px rgba(64, 112, 203, 0.06);
+  padding: 6px 0;
+  margin: 0;
+  color: #63656e;
 }
 .monitor-navigation-admin .nav-item {
-  -webkit-box-flex:0;
-  -ms-flex:0 0 32px;
-  flex:0 0 32px;
-  display:-webkit-box;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-align:center;
-  -ms-flex-align:center;
-  align-items:center;
-  padding:0 20px;
-  list-style:none
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 32px;
+  flex: 0 0 32px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  padding: 0 20px;
+  list-style: none;
 }
 .monitor-navigation-admin .nav-item:hover {
-  color:#3A84FF;
-  cursor:pointer;
-  background-color:#F0F1F5;
+  color: #3a84ff;
+  cursor: pointer;
+  background-color: #f0f1f5;
 }
 .tippy-popper .tippy-tooltip.navigation-message-theme {
-  padding:0;
-  border-radius:0;
-  -webkit-box-shadow:none;
-  box-shadow:none;
+  padding: 0;
+  border-radius: 0;
+  -webkit-box-shadow: none;
+  box-shadow: none;
 }
 </style>
--- src/views/ModelStore/index.vue
+++ src/views/ModelStore/index.vue
@@ -1,23 +1,64 @@
 <template>
   <div id="ModelStore">
     <div id="Selection">
-      <selection title="项目" :list="projectList"></selection>
-      <selection title="后缀名" :list="fileSuffixList"></selection>
+      <selection
+        title="项目"
+        :list="projectList"
+      ></selection>
+      <selection
+        title="后缀名"
+        :list="fileSuffixList"
+      ></selection>
     </div>
     <div id="DataTable">
-      <bk-table style="margin-top: 15px" :data="data" :size="'small'" :pagination="pagination"
-        @page-change="handlePageChange" @page-limit-change="handlePageLimitChange">
-        <bk-table-column type="index" label="ID" width="60"></bk-table-column>
-        <bk-table-column label="文件名" prop="fileName"></bk-table-column>
-        <bk-table-column label="来源" prop="source"></bk-table-column>
-        <bk-table-column label="状态" prop="status"></bk-table-column>
-        <bk-table-column label="创建时间" prop="createTime"></bk-table-column>
-        <bk-table-column label="操作" width="150">
+      <bk-table
+        style="margin-top: 15px"
+        :data="data"
+        :size="'small'"
+        :pagination="pagination"
+        @page-change="handlePageChange"
+        @page-limit-change="handlePageLimitChange"
+      >
+        <bk-table-column
+          type="index"
+          label="ID"
+          width="60"
+        ></bk-table-column>
+        <bk-table-column
+          label="文件名"
+          prop="fileName"
+        ></bk-table-column>
+        <bk-table-column
+          label="来源"
+          prop="source"
+        ></bk-table-column>
+        <bk-table-column
+          label="状态"
+          prop="status"
+        ></bk-table-column>
+        <bk-table-column
+          label="创建时间"
+          prop="createTime"
+        ></bk-table-column>
+        <bk-table-column
+          label="操作"
+          width="150"
+        >
           <template slot-scope="props">
-            <bk-button class="mr10" theme="primary" text :disabled="props.row.status === '上传中'"
-              @click="reset(props.row)">下载
+            <bk-button
+              class="mr10"
+              theme="primary"
+              text
+              :disabled="props.row.status === '上传中'"
+              @click="reset(props.row)"
+            >下载
             </bk-button>
-            <bk-button class="mr10" theme="primary" text @click="remove(props.row)">删除</bk-button>
+            <bk-button
+              class="mr10"
+              theme="primary"
+              text
+              @click="remove(props.row)"
+            >删除</bk-button>
           </template>
         </bk-table-column>
       </bk-table>
@@ -59,9 +100,9 @@ export default {
   created () {
     // get model store information
     axios.get('http://localhost:8000/api/getModelStoreInfo').then(res => {
-      this.data = res.data['modelStoreInfo']
-      this.projectList = res.data['projectList']
-      this.fileSuffixList = res.data['fileSuffixList']
+      this.data = res.data.modelStoreInfo
+      this.projectList = res.data.projectList
+      this.fileSuffixList = res.data.fileSuffixList
     })
   },
   data () {
