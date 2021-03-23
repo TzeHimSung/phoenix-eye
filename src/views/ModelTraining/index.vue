eac25ca452324f1c04c1e16c446db6414c1ed5dd
TzeHimSung <zxc98567@163.com>
2021-03-23 23:18:44 +0800

    feat(modeltraining): add model launch test

--- src/views/ModelTraining/index.vue
+++ src/views/ModelTraining/index.vue
@@ -1,16 +1,35 @@
 <template>
-  <p>{{ haha }}</p>
+  <div>
+    <bk-button
+      class="mr10"
+      theme="primary"
+      @click="launch()"
+    >launch model</bk-button>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { bkButton } from 'bk-magic-vue'
+
 export default {
   name: 'ModelTraining',
   components: {
-    //
+    bkButton
   },
   data () {
     return {
       haha: 'Model training'
+    }
+  },
+  methods: {
+    launch () {
+      const param = {
+        modelname: 'main.py'
+      }
+      axios.post('http://localhost:8000/api/launchtest', param).then((res) => {
+        console.log(res)
+      })
     }
   }
 }
