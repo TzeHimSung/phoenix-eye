5c9e42f66788023504132977bf65c73a98548a10
TzeHimSung <zxc98567@163.com>
2021-03-31 19:26:02 +0800

    feat(modeltraining): add history log query

--- src/views/ModelTraining/index.vue
+++ src/views/ModelTraining/index.vue
@@ -1,77 +1,140 @@
 <template>
-  <div>
-    <bk-table
-      style="margin-top: 15px;"
-      :data="tableData"
-      :size="'small'"
-      :pagination="pagination"
-      @page-change="handlePageChange"
-      @page-limit-change="handlePageLimitChange"
-    >
-      <bk-table-column
-        type="index"
-        label="序列"
-        width="60"
-      ></bk-table-column>
-      <bk-table-column
-        label="进程编号"
-        prop="id"
-      ></bk-table-column>
-      <bk-table-column
-        label="名称"
-        prop="scriptName"
-      ></bk-table-column>
-      <bk-table-column
-        label="创建时间"
-        prop="launchTime"
-      ></bk-table-column>
-      <bk-table-column
-        label="状态"
-        prop="status"
-      ></bk-table-column>
-      <bk-table-column
-        label="操作"
-        width="150"
+  <div id="ModelTraining">
+    <div id="CardContainer">
+      <bk-card
+        title="Running Model"
+        :is-collapse="true"
+        :collapse-status="true"
       >
-        <template slot-scope="props">
-          <bk-button
-            class="mr10"
-            theme="primary"
-            text
-            :disabled="props.row.status === 'Finished' || props.row.status === 'Killed'"
-            @click="killModel(props.row)"
-          >终止</bk-button>
-        </template>
-      </bk-table-column>
-    </bk-table>
+        <div id="DataTable">
+          <bk-table
+            style="margin-top: 15px;"
+            :data="runningModelData"
+            :size="'small'"
+            :pagination="runningModelDataPagination"
+            v-bkloading="{ isLoading: runningModelDataLoading, zIndex: 10 }"
+            @page-change="handlePageChange"
+            @page-limit-change="handlePageLimitChange"
+          >
+            <bk-table-column
+              type="index"
+              label="序列"
+              width="60"
+            ></bk-table-column>
+            <bk-table-column
+              label="进程编号"
+              prop="id"
+            ></bk-table-column>
+            <bk-table-column
+              label="名称"
+              prop="scriptName"
+            ></bk-table-column>
+            <bk-table-column
+              label="创建时间"
+              prop="launchTime"
+            ></bk-table-column>
+            <bk-table-column
+              label="状态"
+              prop="status"
+            ></bk-table-column>
+            <bk-table-column
+              label="操作"
+              width="150"
+            >
+              <template slot-scope="props">
+                <bk-button
+                  class="mr10"
+                  theme="primary"
+                  text
+                  :disabled="props.row.status === 'Finished' || props.row.status === 'Killed'"
+                  @click="killModel(props.row)"
+                >终止</bk-button>
+              </template>
+            </bk-table-column>
+          </bk-table>
+        </div>
+      </bk-card>
+    </div>
+    <div id="CardContainer">
+      <bk-card
+        title="Finished Model"
+        :is-collapse="true"
+        :collapse-status="true"
+      >
+        <div id="DataTable">
+          <bk-table
+            style="margin-top: 15px;"
+            :data="finishedModelData"
+            :size="'small'"
+            :pagination="finishedModelDataPagination"
+            v-bkloading="{ isLoading: finishedModelDataLoading, zIndex: 10 }"
+            @page-change="handlePageChange"
+            @page-limit-change="handlePageLimitChange"
+          >
+            <bk-table-column
+              type="index"
+              label="序列"
+              width="60"
+            ></bk-table-column>
+            <bk-table-column
+              label="名称"
+              prop="scriptName"
+            ></bk-table-column>
+            <bk-table-column
+              label="创建时间"
+              prop="launchTime"
+            ></bk-table-column>
+            <bk-table-column
+              label="状态"
+              prop="status"
+            ></bk-table-column>
+          </bk-table>
+        </div>
+      </bk-card>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import { bkTable, bkTableColumn } from 'bk-magic-vue'
+import { bkTable, bkTableColumn, bkCard } from 'bk-magic-vue'
 
 export default {
   name: 'ModelTraining',
   components: {
     bkTable,
-    bkTableColumn
+    bkTableColumn,
+    bkCard
   },
   created () {
     axios.get('http://localhost:8000/api/getModelTrainingInfo').then((res) => {
-      console.log(res)
-      this.tableData = res.data.data
+      // update running model data table
+      this.runningModelData = res.data.runningModelList
+      this.runningModelDataPagination.count = this.runningModelData.length
+      this.runningModelDataLoading = false
+      // update finished model data table
+      this.finishedModelData = res.data.finishedModelList
+      this.finishedModelDataPagination.count = this.finishedModelData.length
+      this.finishedModelDataLoading = false
     })
   },
   data () {
     return {
       haha: 'Model training',
-      tableData: [],
-      pagination: {
+      runningModelData: [],
+      finishedModelData: [],
+      runningModelDataPagination: {
         current: 1,
         count: 500,
         limit: 20
-      }
+      },
+      finishedModelDataPagination: {
+        current: 1,
+        count: 500,
+        limit: 20
+      },
+      runningModelDataLoading: true,
+      finishedModelDataLoading: true
     }
   },
   methods: {
@@ -103,3 +166,15 @@ export default {
   }
 }
 </script>
+
+<style>
+#CardContainer {
+  margin-top: 20px;
+  margin-left: 18%;
+  margin-right: 18%;
+}
+
+#DataTable {
+  margin: 3px;
+}
+</style>
