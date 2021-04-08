5fa2481968703f82da124c46440616cb5bfc7305
TzeHimSung <zxc98567@163.com>
2021-04-08 16:23:36 +0800

    feat(router): add home page route configuration

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 // import pages
-import Home from '../views/Home.vue'
+import Home from '@/views/HomePage'
 import DataStore from '@/views/DataStore'
 import ModelStore from '@/views/ModelStore'
 import ModelTraining from '@/views/ModelTraining'
